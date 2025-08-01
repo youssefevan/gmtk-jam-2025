2ae9175ee534bdf4555bb63a4d0fc46e2b61ab4b
Evan <evanyoussef8@gmail.com>
2025-08-01 18:30:37 -0400

    lerp on board

--- gmtk-jam-2025/scripts/board.gd
+++ gmtk-jam-2025/scripts/board.gd
@@ -3,5 +3,13 @@ extends Node2D
 var board_pos = 0
 
 func _on_button_pressed() -> void:
+	var roll = randi_range(1, 6)
+	$Button.text = str(roll)
 	
-	$Path2D/PathFollow2D.progress += 25
+	print($Path2D.curve.get_point_position(0))
+	
+	var tween = get_tree().create_tween()
+	tween.tween_property($Path2D/PathFollow2D, "progress", $Path2D/PathFollow2D.progress + 25 * roll, 0.2 * roll)
+	$Button.disabled = true
+	await tween.finished
+	$Button.disabled = false
