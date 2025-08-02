fab945f6c9593773f5cb3b6fa0b16963e83c2732
Evan <evanyoussef8@gmail.com>
2025-08-01 20:03:35 -0400

    move player token around board, stopping at each space

--- gmtk-jam-2025/scripts/board.gd
+++ gmtk-jam-2025/scripts/board.gd
@@ -4,12 +4,22 @@ var board_pos = 0
 
 func _on_button_pressed() -> void:
 	var roll = randi_range(1, 6)
-	$Button.text = str(roll)
+	$Number.text = str(roll)
 	
-	print($Path2D.curve.get_point_position(0))
-	
-	var tween = get_tree().create_tween()
-	tween.tween_property($Path2D/PathFollow2D, "progress", $Path2D/PathFollow2D.progress + 25 * roll, 0.2 * roll)
 	$Button.disabled = true
-	await tween.finished
+	
+	await get_tree().create_timer(0.3).timeout
+	
+	for i in range(roll):
+		var tween = get_tree().create_tween()
+		tween.set_trans(Tween.TRANS_CUBIC)
+		tween.tween_property(
+			$Path2D/player,
+			"global_position",
+			$Path2D.curve.get_point_position((board_pos % $Path2D.curve.point_count) + (i+1)),
+			0.2
+		)
+		await tween.finished
+	
+	board_pos += roll
 	$Button.disabled = false
