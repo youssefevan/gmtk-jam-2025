cfb38404ed57485ac90ca451967e2a87a66327be
Evan <evanyoussef8@gmail.com>
2025-08-02 17:48:36 -0400

    Card tile, random tiles, visual changes

--- gmtk-jam-2025/scripts/board.gd
+++ gmtk-jam-2025/scripts/board.gd
@@ -1,18 +1,35 @@
 extends Node2D
 
 @onready var tile_scene = preload("res://scenes/tile.tscn")
+@export var suits : Array[Texture2D]
 
 var board_pos = 0
 
 func _ready():
 	Global.connect("end_combat", start_turn)
 	
-	for point in range($Path2D.curve.point_count):
+	set_tiles()
+
+func set_tiles():
+	for point in range($Path2D.curve.point_count-1):
 		$Path2D.curve.set_point_position(point, $Path2D.curve.get_point_position(point) + global_position)
 		
+		
 		var tile = tile_scene.instantiate()
+		if point == 0:
+			tile.type = "boss"
+		else:
+			match Global.rng.randi_range(0, 2):
+				0:
+					tile.type = "card"
+				1:
+					tile.type = "chance"
+				2:
+					tile.type = "enemy"
+		
 		$Tiles.add_child(tile)
 		tile.global_position = $Path2D.curve.get_point_position(point)
+
 
 func start_turn():
 	var tween = get_tree().create_tween()
@@ -54,3 +71,30 @@ func _on_button_pressed() -> void:
 		tween2.tween_property($Path2D/player/Sprite2D, "modulate", Color("1b908f"), 0.5)
 		
 		Global.enter_combat()
+	elif current_tile.type == "boss":
+		var tween = get_tree().create_tween()
+		tween.tween_property($boardTexture, "modulate", Color("ff4726"), 0.5)
+		
+		var tween2 = get_tree().create_tween()
+		tween2.tween_property($Path2D/player/Sprite2D, "modulate", Color("1b908f"), 0.5)
+		
+		Global.enter_combat()
+	elif current_tile.type == "card":
+		var suit = Global.rng.randi_range(0, 4)
+		match suit:
+			0:
+				Global.deck_comp["Physical"]
+			1:
+				Global.deck_comp["Freeze"]
+			2:
+				Global.deck_comp["Heal"]
+			3:
+				Global.deck_comp["Fire"]
+			4:
+				Global.deck_comp["Poison"]
+		
+		$AddCard.visible = true
+		$AddCard/Card/Suit.texture = suits[suit]
+		await get_tree().create_timer(1).timeout
+		$AddCard.visible = false
+		start_turn()
--- gmtk-jam-2025/scripts/tile.gd
+++ gmtk-jam-2025/scripts/tile.gd
@@ -7,6 +7,7 @@ var tiles = {
 	"card": 0,
 	"chance": 1,
 	"enemy": 2,
+	"boss": 3,
 }
 
 func _ready():
